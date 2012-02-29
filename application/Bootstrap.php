3f235060661fa876daa38e9224831c43b591e413
Ricardo Cortes <ricardo@rcortes.(none)>
2012-02-29 17:47:00 -0600

    Cambios al bootstrap principal

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,9 +9,10 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		$view = new Zend_View($this->getOptions()); //get the view object
 		//add the jquery view helper path into your project
 		$view->addHelperPath("ZendX/JQuery/View/Helper", "ZendX_JQuery_View_Helper");
+		
 		$base_url = substr($_SERVER['PHP_SELF'], 0, -9);
+		
 		//jquery lib includes here (default loads from google CDN)
-		//Zend_Debug::dump($base_url);die;
 		$view->jQuery()	->enable()//enable jquery ; ->setCdnSsl(true) if need to load from ssl location
 						->setLocalPath($base_url .'js/jquery/jquery-1.7.1.min.js')
 						->setUiLocalPath($base_url .'js/jquery/jquery-ui-1.8.13.custom.min.js')				
